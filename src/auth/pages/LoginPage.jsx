647fed9c437ee7bb7baecfea141626f22404827c
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-31 18:48:38 +0200

    update changes

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,31 @@
+import { Grid } from '@mui/material';
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{
+          minHeight: '100vh',
+          backgroundColor: 'primary.main',
+          padding: 4,
+        }}
+      >
+        <Grid
+          item
+          className="box-shadow"
+          xs={3}
+          sx={{
+            backgroundColor: 'white',
+            padding: 3,
+            borderRadius: 2,
+          }}
+        ></Grid>
+      </Grid>
+    </>
+  );
 };
