7dd70c3dc3a531063d0054d824e06a860dc1ec0e
Laura Portillo <lauraportillo.ark@gmail.com>
2022-09-09 16:23:51 +0200

    add RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,41 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="Full Name" type="text" placeholder="Your Name" fullWidth />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="email" type="email" placeholder="email@google.com" fullWidth />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField label="password" type="password" placeholder="password" fullWidth />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth>
+                Create an account
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>Do you have an account?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Go
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
