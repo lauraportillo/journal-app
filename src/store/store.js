e6ab5775a5feea04920ee159ecba7731a2a6c2aa
Laura Portillo <lauraportillo.ark@gmail.com>
2022-09-30 16:00:25 +0200

    add slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { authSlice } from './auth';
 
 export const store = configureStore({
     reducer: {
+        auth: authSlice.reducer,
 
     },
 });
