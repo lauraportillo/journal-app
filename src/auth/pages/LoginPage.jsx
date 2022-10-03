aa8e006c79536d3651550f81703755cfc18f809d
Laura Portillo <lauraportillo.ark@gmail.com>
2022-10-03 15:58:40 +0200

    add login with redux

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,18 +1,36 @@
+import { useDispatch } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+
 import { AuthLayout } from '../layout/AuthLayout';
+
 import { useForm } from '../../hooks/useForm';
 
+import { checkingAuthentication } from '../../store/auth/thunks';
+
 export const LoginPage = () => {
+  const dispatch = useDispatch();
+
   const { email, password, onInputChange } = useForm({
     email: 'laura@google.com',
     password: '123456',
   });
 
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    dispatch(checkingAuthentication());
+
+    console.log({ email, password });
+  };
+
+  const onGoogleSignIn = () => {
+    console.log('onGoogleSignIn');
+  };
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -40,13 +58,13 @@ export const LoginPage = () => {
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Login
               </Button>
             </Grid>
 
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" fullWidth onClick={onGoogleSignIn}>
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
---
+++ src/store/auth/thunks.js
@@ -0,0 +1,8 @@
+import { checkingCredentials } from "./";
+
+export const checkingAuthentication = (email, password) => {
+    return async (dispatch) => {
+        dispatch(checkingCredentials());
+    }
+
+}
