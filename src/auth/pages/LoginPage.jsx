68aa00eea820dff676ac056d37078e54b24d6aaa
Laura Portillo <lauraportillo.ark@gmail.com>
2022-10-03 13:22:44 +0200

    add useForm from my custom hooks

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,18 +2,40 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
 
 export const LoginPage = () => {
+  const { email, password, onInputChange } = useForm({
+    email: 'laura@google.com',
+    password: '123456',
+  });
+
   return (
     <AuthLayout title="Login">
       <form>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="email" type="email" placeholder="email@google.com" fullWidth />
+            <TextField
+              label="email"
+              type="email"
+              placeholder="email@google.com"
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+            />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="password" type="password" placeholder="password" fullWidth />
+            <TextField
+              label="password"
+              type="password"
+              placeholder="password"
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+            />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
