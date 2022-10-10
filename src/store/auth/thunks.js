e530e6663504a72ed4026026c69e45ef4e6b9f7c
Laura Portillo <lauraportillo.ark@gmail.com>
2022-10-10 14:18:04 +0200

    add google sign in

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { singInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials } from "./";
 
 
@@ -13,5 +14,8 @@ export const checkingAuthentication = (email, password) => {
 export const startGoogleSignIn = () => {
     return async (dispatch) => {
         dispatch(checkingCredentials());
+
+        const result = await singInWithGoogle();
+        console.log(result);
     }
 }
