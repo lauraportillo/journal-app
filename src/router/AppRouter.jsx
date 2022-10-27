405964e6846729076872c3dc2ea0cf828a76264e
Laura Portillo <lauraportillo.ark@gmail.com>
2022-10-27 16:24:01 +0200

    add loader

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,17 @@
+import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { CheckingAuthLoader } from '../ui';
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === 'checking') {
+    return <CheckingAuthLoader />;
+  }
+
   return (
     <Routes>
       <Route path="/auth/*" element={<AuthRoutes />} />
