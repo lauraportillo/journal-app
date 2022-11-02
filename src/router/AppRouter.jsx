8637419019a571a729a326ce6bfec459b14c150e
Laura Portillo <lauraportillo.ark@gmail.com>
2022-11-02 13:51:03 +0100

    keep authentiction state when reload

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,30 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import { FirebaseAuth } from '../firebase/config';
 
+import { login, logout } from '../store/auth';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
+
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
 import { CheckingAuthLoader } from '../ui';
 
 export const AppRouter = () => {
   const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Firebase tiene esta fución que está pendiente de los cambios de usuario
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      // Si no hay ningún usuario, mando llamar al logout:
+      if (!user) return dispatch(logout());
+
+      // Si tengo un usuario:
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
 
   if (status === 'checking') {
     return <CheckingAuthLoader />;
