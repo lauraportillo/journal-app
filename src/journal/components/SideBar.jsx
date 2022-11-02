f05116f80d08786f3298ef029e0cedfd28ca6662
Laura Portillo <lauraportillo.ark@gmail.com>
2022-11-02 16:09:31 +0100

    add firebase logout

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -12,8 +12,11 @@ import {
   Typography,
 } from '@mui/material';
 import { TurnedInNot } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
 
 export const SideBar = ({ drawerWidth = 240 }) => {
+  const { displayName } = useSelector((state) => state.auth);
+
   return (
     <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
       <Drawer
@@ -26,7 +29,7 @@ export const SideBar = ({ drawerWidth = 240 }) => {
       >
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-            Laura Portillo
+            {displayName}
           </Typography>
         </Toolbar>
         <Divider />
